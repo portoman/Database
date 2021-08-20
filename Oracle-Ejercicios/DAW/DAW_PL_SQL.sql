c30ca1de44441d6bafd03096021a5a0922e947e7
portobuj <portbuj@yahoo.es>
2021-08-20 13:33:25 +0200

    Update 20210820

--- Oracle-Ejercicios/DAW/DAW_PL_SQL.sql
+++ Oracle-Ejercicios/DAW/DAW_PL_SQL.sql
@@ -111,3 +111,199 @@ declare
                             || ' y los comentarios son '|| v_pedido.comentarios
                             );
     end;
+
+#10. Realizar una función que me devuelva la suma de pagos que ha realizado. Pasa el código por parámetro.
+
+/*Creando la función*/
+create or replace function Pagos_cliente(v_codigocliente clientes.codigocliente%type)
+return Number 
+as 
+    v_sumapagos pagos.cantidad%type :=0;
+begin
+
+    select sum(cantidad) into v_sumapagos
+    from pagos
+    where codigocliente=v_codigocliente;
+
+    return v_sumapagos;
+
+end;
+
+/*Usando la función*/
+declare 
+    v_codigocliente pedidos.codigocliente%type := &codigo;
+    v_suma pagos.cantidad%type;
+begin
+    v_suma := Pagos_cliente(v_codigocliente);
+    DBMS_OUTPUT.PUT_LINE('La suma de pagos es ' || v_suma);
+
+end;
+
+#11. Realizar un método o procedimiento que muestre el total en euros de un pedido, pasale el código por parámetro.
+
+/*Creando el procedimiento*/
+create or replace procedure total_pedido(v_codigopedido pedidos.codigopedido%type)
+as 
+    v_total number(8) :=0;
+begin
+
+    select sum(dp.cantidad * dp.preciounidad) into v_total
+    from pedidos p, detallepedidos dp
+    where p.codigopedido=dp.codigopedido and p.codigopedido=v_codigopedido;
+
+    DBMS_OUTPUT.PUT_LINE('EL pedido total es  ' || v_total);
+
+end;
+
+/*Usando el procedimiento*/
+declare 
+    v_codigopedido pedidos.codigopedido%type := &codigo;
+begin
+    total_pedido(v_codigopedido);
+end;
+
+
+#12. Mostrar el nombre de un cliente dado su código. Controla en caso de que no se encuentre, mostrando un mensaje por ejemplo
+
+ declare
+        v_codigocliente clientes.codigocliente%type := &codigo;
+        v_nombrecliente clientes.nombrecliente%type;
+    begin
+        select nombrecliente into v_nombrecliente
+        from clientes
+        where codigocliente=v_codigocliente;
+
+        DBMS_OUTPUT.PUT_LINE('El nombre del cliente es '|| v_nombrecliente);
+        exception
+         when no_data_found then 
+            DBMS_OUTPUT.PUT_LINE('El cliente no existe');
+    end;
+
+#13. Realizar una función que me devuelva la suma de pagos que ha realizado. Pasa el código por parámetro . 
+Controla en caso de que no se encuentre, en ese caso devuelve un -1.
+
+create or replace function Pagos_cliente(v_codigocliente clientes.codigocliente%type)
+return Number 
+as 
+    v_sumapagos pagos.cantidad%type :=0;
+begin
+
+    select sum(cantidad) into v_sumapagos
+    from pagos
+    where codigocliente=v_codigocliente;
+
+    if v_sumapagos is null then
+        raise no_data_found;
+    else
+        return v_sumapagos;
+    end if;
+
+    exception
+            when no_data_found then 
+                return -1;
+end;
+/
+declare 
+    v_codigocliente pedidos.codigocliente%type := &codigo;
+    v_suma pagos.cantidad%type;
+begin
+    v_suma := Pagos_cliente(v_codigocliente);
+    if v_suma =-1 then 
+    DBMS_OUTPUT.PUT_LINE('El cliente no existe');
+    else
+    DBMS_OUTPUT.PUT_LINE('La suma de pagos es ' || v_suma);
+    end if;
+end;
+
+#14. Realizar un método o procedimiento que muestre el total en euros de un pedido, pásale el código por parámetro.
+Controla en caso de que no se encuentre, en ese caso devuelve un 0. Pásale otro parámetro, si supera ese límite, lanzaremos una 
+excepción propia y devolveremos un 0.
+
+create or replace function total_pedido_f(v_codigopedido pedidos.codigopedido%type, limite number)
+return number
+as 
+    v_total number(8) :=0;
+    limite_superado exception;
+begin
+
+    select sum(dp.cantidad * dp.preciounidad) into v_total
+    from pedidos p, detallepedidos dp
+    where p.codigopedido=dp.codigopedido and p.codigopedido=v_codigopedido;
+
+    if v_total is null then
+        raise no_data_found;
+    else
+        if v_total > limite then 
+            raise limite_superado;
+            return v_total;
+        else
+            return v_total;
+        end if;
+    end if;
+
+exception 
+    when no_data_found then
+        DBMS_OUTPUT.PUT_LINE('No existe el pedido');
+        return 0;
+
+    when limite_superado then
+        DBMS_OUTPUT.PUT_LINE('Se ha superado el límite');
+        return 0;
+end;
+
+/*Usando el procedimiento*/
+declare 
+    v_codigopedido pedidos.codigopedido%type := &codigo;
+    v_limite number(8) := &limite;
+    v_total number(8);
+begin
+    v_total := total_pedido_f(v_codigopedido, v_limite);
+    DBMS_OUTPUT.PUT_LINE('El pedido es de '|| v_total);
+end;
+
+#15. Crea una función a la que le pasaremos como parámetros de entrada: MATRICULA, NUEVO_PRECIO_COMPRA.
+La función modificará los datos del coche que tenga la matrícula introducida, actualizando el PRECIO_COMPRA
+de la siguiente forma:
+
+    - Si PRECIO_COMPRA es nulo: Hacer un update en el campo PRECIO_COMPRA asignándole el valor de  NUEVO_PRECIO_COMPRA
+
+    - Si no: Hacer un update en el campo precio_compra asignándole el valor de precio_compra+(PRECIO_COMPRA-NUEVO_PRECIO_COMPRA)
+
+La función devolverá el número de filas actualizadas
+Crea un bloque anónimo que ejecuta la función anterior y muestre el resultado devuelto por la función
+
+
+create or replace function actualizaPrecioCoche(
+    v_matricula concesionario.coche.matricula%type, 
+    v_nuevo_precio_compra coche.precio_compra%type)
+return number
+as
+    v_precio_compra coche.precio_compra%type;
+begin
+
+    select precio_compra into v_precio_compra
+    from coche where matricula=v_matricula;
+
+    if v_precio_compra is null then
+        update coche
+        set precio_compra=v_nuevo_precio_compra
+        where matricula=v_matricula;
+    else
+        update coche
+        set precio_compra=precio_compra+(precio_compra-v_nuevo_precio_compra)
+        where matricula=v_matricula;
+    end if;
+
+    return SQL%ROWCOUNT;
+end;
+
+/
+
+declare
+    v_matricula coche.matricula%type := &matricula;
+    v_nuevo_precio_compra coche.precio_compra%type :=&nuevo_precio;
+    v_total_filas number(8);
+begin
+    v_total_filas := actualizaPrecioCoche(v_matricula, v_nuevo_precio_compra);
+    DBMS_OUTPUT.PUT_LINE('Se han modificado '||v_total_filas || ' filas');
+end
